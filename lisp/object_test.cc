ba2986df9f3abda6c440930e91c6d0656d6eaa6f
Masayuki Takagi <kamonama@gmail.com>
2018-09-10 23:32:23 +0900

    Refactor object tests

--- lisp/object_test.cc
+++ lisp/object_test.cc
@@ -3,72 +3,64 @@
 #include <gtest/gtest.h>
 
 
-/*
- * Null
- */
-
-TEST(NullTest, NullTest) {
-    EXPECT_TRUE(null(nil));
-    EXPECT_FALSE(null(make_int(1)));
+TEST(ObjectTest, NullTest) {
+    {
+        EXPECT_TRUE(null(nil));
+    }
+    {
+        EXPECT_FALSE(null(make_int(1)));
+    }
 }
 
-
-/*
- * Int
- */
-
-TEST(IntTest, IntTest) {
-    object_t x = make_int(1);
-    EXPECT_TRUE(is_int(x));
-    EXPECT_FALSE(is_int(nil));
-    EXPECT_EQ(int_value(x), 1);
+TEST(ObjectTest, IntTest) {
+    {
+        object_t x = make_int(1);
+        EXPECT_TRUE(is_int(x));
+        EXPECT_EQ(int_value(x), 1);
+    }
+    {
+        EXPECT_FALSE(is_int(nil));
+    }
 }
 
-
-/*
- * Cons
- */
-
-TEST(ConsTest, IsConsTest) {
+TEST(ObjectTest, ConsTest) {
+    {
+        object_t x = make_cons(make_int(1), nil);
+        EXPECT_TRUE(is_cons(x));
+        EXPECT_EQ(car(x), make_int(1));
+        EXPECT_EQ(cdr(x), nil);
+    }
+    {
+        EXPECT_FALSE(is_cons(nil));
+    }
+    {
+        EXPECT_EQ(car(nil), nil);
+        EXPECT_EQ(cdr(nil), nil);
+    }
 }
 
-TEST(ConsTest, CarTest) {
-/*
-    EXPECT_EQ(car(nil), nil);
-    EXPECT_EQ(car(t), nil);
-    EXPECT_NE(t, nil);
-*/
+TEST(ObjectTest, SymbolTest) {
+    {
+        object_t x = make_symbol("FOO");
+        EXPECT_TRUE(is_symbol(x));
+        EXPECT_STREQ(symbol_name(x), "FOO");
+    }
+    {
+        object_t x = make_symbol("FOO");
+        object_t y = make_symbol("FOO");
+        EXPECT_NE(x, y);
+    }
+    {
+        EXPECT_TRUE(is_symbol(nil));
+    }
+    {
+        object_t x = make_int(1);
+        EXPECT_FALSE(is_symbol(x));
+    }
+    {
+        EXPECT_STREQ(symbol_name(nil), "NIL");
+    }
 }
-
-
-/*
- * Symbol
- */
-
-TEST(SymbolTest, MakeSymbolTest) {
-    object_t x = make_symbol("FOO");
-    EXPECT_TRUE(is_symbol(x));
-    EXPECT_STREQ(symbol_name(x), "FOO");
-
-    object_t y = make_symbol("FOO");
-    EXPECT_NE(x, y);
-}
-
-TEST(SymbolTest, IsSymbolTest) {
-    EXPECT_TRUE(is_symbol(nil));
-
-    object_t x = make_int(1);
-    EXPECT_FALSE(is_symbol(x));
-}
-
-TEST(SymbolTest, SymbolNameTest) {
-    EXPECT_STREQ(symbol_name(nil), "NIL");
-}
-
-
-/*
- * String
- */
 
 TEST(ObjectTest, StringTest) {
     {
