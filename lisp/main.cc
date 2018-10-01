5fbc83ec033481928c3eb8edd750d97d2827515e
Masayuki Takagi <kamonama@gmail.com>
2018-10-01 13:55:00 +0900

    Add repl()

--- lisp/main.cc
+++ lisp/main.cc
@@ -1,3 +1,22 @@
+#include <iostream>
+#include <string>
+
+#include "lisp/eval.h"
+#include "lisp/prin1.h"
+#include "lisp/read.h"
+
+
+void repl() {
+    std::string line;
+    do {
+        std::cout << "> ";
+        std::getline(std::cin, line);
+        prin1(eval(read(line.c_str())));
+        std::cout << std::endl;
+    } while (true);
+}
+
 int main(int argc, char* argv[]) {
+    repl();
     return 0;
 }
